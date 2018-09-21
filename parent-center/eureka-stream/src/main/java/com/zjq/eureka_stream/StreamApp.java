16722b3219d6b91f494b5d8767bda8606ba8f8ed
zhangjunquan <18271632480@163.com>
2018-09-21 14:27:56 +0800

    新增spring cloud stream 消息驱动微服务

---
+++ parent-center/eureka-stream/src/main/java/com/zjq/eureka_stream/StreamApp.java
@@ -0,0 +1,11 @@
+package com.zjq.eureka_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApp {
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApp.class, args);
+	}
+}
