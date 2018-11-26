7793f417dae440553f18fe80b23bfa5ac6fbdcc3
zhangjunquan <18271632480@163.com>
2018-11-26 16:24:59 +0800

    新增canal同步工具

---
+++ parent-center/eureka-canal/src/main/java/com/zjq/eureka_canal/CanalApp.java
@@ -0,0 +1,18 @@
+package com.zjq.eureka_canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CanalApp 
+{
+	 public static void main( String[] args )
+	    {
+	    	SpringApplication.run(CanalApp.class, args);
+	    }
+}
