8f2b1872c81f6a32a05085d5c268bc1d20463904
zhangjunquan <18271632480@163.com>
2018-11-29 10:13:15 +0800

    canal模块部分代码删除

--- parent-center/eureka-canal/src/main/java/com/zjq/eureka_canal/CanalApp.java
+++ parent-center/eureka-canal/src/main/java/com/zjq/eureka_canal/CanalApp.java
@@ -3,16 +3,11 @@ package com.zjq.eureka_canal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
-public class CanalApp 
-{
-	 public static void main( String[] args )
-	    {
-	    	SpringApplication.run(CanalApp.class, args);
-	    }
+//@EnableElasticsearchRepositories(basePackages = "com.zjq.eureka_canal.elasticsearch.dao")
+public class CanalApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CanalApp.class, args);
+		
+	}
 }
